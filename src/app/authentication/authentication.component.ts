e3e6017b65586591e5017957ef216962655cdacf
Shafi <shafisahal99@gmail.com>
2021-04-14 07:49:17 +0530

    Renamed color variables

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./authentication.component.scss']
 })
 export class AuthenticationComponent implements OnInit {
+  isLogin = true;
 
   constructor() { }
 
