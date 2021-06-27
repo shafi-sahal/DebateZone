221bc2d14068ec9b96d2888fd6f7a81ac4971c4c
ShafiSahal99 <shafisahal99@gmail.com>
2021-06-27 15:59:51 +0530

    Remove unwanted code

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit} from '@angular/core';
-import { Subject, Subscription } from 'rxjs';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Spinner } from '../shared/components/spinner/spinner.service';
 
 @Component({
@@ -8,20 +8,10 @@ import { Spinner } from '../shared/components/spinner/spinner.service';
   styleUrls: ['./authentication.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AuthenticationComponent implements OnInit, OnDestroy {
+export class AuthenticationComponent {
   isSignUp = new Subject<boolean>();
-  classBodyHeight = 'normal-height';
-  private subscriptions = new Subscription();
 
   constructor(private spinner: Spinner) { this.spinner.hide(); }
 
-  ngOnInit(): void {
-    this.subscriptions.add(this.isSignUp.subscribe(isSignUp =>
-      this.classBodyHeight = isSignUp ? 'long-height' : 'normal-height')
-    );
-  }
-
   setIsSignup(isSignUp: boolean): void { this.isSignUp.next(isSignUp); }
-
-  ngOnDestroy(): void { this.subscriptions.unsubscribe(); }
 }
