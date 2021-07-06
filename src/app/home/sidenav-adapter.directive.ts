4e81c618c3c15b3777c37438a342843e8cea9e02
ShafiSahal99 <shafisahal99@gmail.com>
2021-07-06 21:03:53 +0530

    Fix sidenav closing while rotating mobile

--- src/app/home/sidenav-adapter.directive.ts
+++ src/app/home/sidenav-adapter.directive.ts
@@ -1,8 +1,11 @@
 import { AfterViewInit, Directive, HostListener} from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { screenSizes } from '../../assets/screen-sizes';
 
 @Directive({ selector: '[appAdaptSideNavToDevice]' })
 export class SideNavRenderer implements AfterViewInit {
+  private previousWindowWidth = 0;
+
   constructor (private sidenav: MatSidenav) {}
 
   ngAfterViewInit(): void {
@@ -11,15 +14,17 @@ export class SideNavRenderer implements AfterViewInit {
 
   @HostListener('window:resize', ['$event'])
   private adaptSideNavToDevice(): void {
+    const windowWidth = window.innerWidth;
     const buttonMenu = document.getElementById('button-menu');
-    if (window.innerWidth < 768) {
+    if (windowWidth < screenSizes.desktopWidth) {
       this.sidenav.mode = 'over';
-      this.sidenav.close();
+      if (this.previousWindowWidth >= screenSizes.desktopWidth) { this.sidenav.close(); }
       buttonMenu?.removeAttribute('hidden');
     } else {
       this.sidenav.mode = 'side';
       this.sidenav.open();
       buttonMenu?.setAttribute('hidden', 'true');
     }
+    this.previousWindowWidth = windowWidth;
   }
 }
---
+++ src/assets/screen-sizes.ts
@@ -0,0 +1,1 @@
+export const screenSizes = { desktopWidth: 768 };
