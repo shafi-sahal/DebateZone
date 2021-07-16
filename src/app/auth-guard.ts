85e4d6573a2108ecb9b1b53a6aeed8df04c19e4f
ShafiSahal99 <shafisahal99@gmail.com>
2021-07-16 21:23:39 +0530

    chore: Create endpoint to get name and username

--- src/app/auth-guard.ts
+++ src/app/auth-guard.ts
@@ -1,15 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { SessionService } from './session.service';
+import { User } from './shared/models/user.model';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private sessionService: SessionService, private router: Router) {}
+  constructor(private sessionService: SessionService, private router: Router, private http: HttpClient) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+    boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
     const authenticated = this.sessionService.authenticated;
     if (!authenticated) this.router.navigate(['authentication']);
+    if (!this.sessionService.user) {
+      this.http.get<{ user: User }>(environment.apiUrl + '/user').subscribe(response => this.sessionService.user = response.user);
+    }
     return authenticated;
   }
 }
