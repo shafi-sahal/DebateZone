063decb3bf91e003b52776df1cd84dfcb6b7ea74
ShafiSahal99 <shafisahal99@gmail.com>
2021-07-22 20:52:57 +0530

    feat: Add account module

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 
-const routes: Routes = [ { path: '', component: HomeComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [ { path: 'account', loadChildren: () => import('../account/account.module').then(m => m.AccountModule) } ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
