9c45ed3b5e09db24068ea9102eafbf2c24818ce6
ShafiSahal99 <shafisahal99@gmail.com>
2021-07-23 14:54:49 +0530

    chore: Update clickedNavButton index on route changes

--- src/app/home/nav.service.ts
+++ src/app/home/nav.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Injectable()
-export class NavService {
+export class NavService  {
   navButtons: { label: string, icon: string, route: string }[] = [
     { label: 'Debates', icon: 'forum', route: '' }, { label: 'Account', icon: 'person' , route: 'account'}
   ]
   clickedNavbuttonIndex = 0;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (!(event instanceof NavigationEnd)) return;
+      const route = router.url.replace('/', '');
+      const buttonIndex = this.navButtons.findIndex(button => button.route === route);
+      this.clickedNavbuttonIndex = buttonIndex;
+    });
+  }
 }
