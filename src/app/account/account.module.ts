895d290a70eb0ba97f2ff5aadbafc820f67134bc
ShafiSahal99 <shafisahal99@gmail.com>
2021-07-23 19:50:28 +0530

    chore: Add matcard for account component

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountComponent } from './account.component';
 import { AccountRoutingModule } from './account-routing.module';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AccountRoutingModule } from './account-routing.module';
   ],
   imports: [
     CommonModule,
-    AccountRoutingModule
+    AccountRoutingModule,
+    MatCardModule
   ]
 })
 export class AccountModule { }
