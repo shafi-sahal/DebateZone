ca9f7eb8b0f60843f8504f8ee0676d23f54793cd
ShafiSahal99 <shafisahal99@gmail.com>
2021-08-03 21:08:35 +0530

    chore: Add method to fetch user

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../shared/models/user.model';
+
+const BACKEND_URL = environment.apiUrl + '/user';
 
 @Injectable()
 export class AccountService {
   user: User = { name: '', username: '', email: '', mobile: '' };
-  isDuplicateUsername: boolean | undefined;
-  isDuplicateEmail: boolean | undefined;
+
+  constructor(private http: HttpClient) {}
+
+  fetchUser(): Observable<User> {
+    return this.http.get<{ user: User }>(BACKEND_URL).pipe(
+      map(response => {
+        this.user = response.user;
+        return response.user;
+      })
+    );
+  }
 }
