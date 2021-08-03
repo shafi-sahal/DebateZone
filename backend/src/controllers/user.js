de7640cc57a64d766a39f35aa1f4efcd25c5797b
ShafiSahal99 <shafisahal99@gmail.com>
2021-08-03 15:27:41 +0530

    chore: Add server erroe message for duplicate fields on signup

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -20,8 +20,20 @@ exports.createUser = (req, res) => {
     const token = generateToken({ userId: user.id });
     res.status(201).json({ token: token, user: { name: user.name, username: user.username }});
   })
-  .catch(error => errorHandler(res, error)
-  );
+  .catch(error => {
+    if (!error.name === 'SequelizeUniqueConstraintError') return errorHandler(res, error)
+    console.log(error);
+    const duplicateField = error.errors[0].path.split('.')[1];
+    let message = ''
+    if (duplicateField === 'username'){
+      message = 'Username already exists. Try again';
+    } else if (duplicateField === 'email') {
+      message = 'An account with the same email already exists';
+    } else if (duplicateField === 'mobile') {
+      message = 'An account with same mobile number already exists'
+    }
+    res.status(400).json({ message: message });
+  });
 }
 
 exports.login = (req, res) => {
