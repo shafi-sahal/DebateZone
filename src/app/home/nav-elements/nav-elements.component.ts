ec0ddccba4feb89de259244b758197e2fcbe691a
ShafiSahal99 <shafisahal99@gmail.com>
2021-08-03 14:16:35 +0530

    chore: Remove reloading on logout

--- src/app/home/nav-elements/nav-elements.component.ts
+++ src/app/home/nav-elements/nav-elements.component.ts
@@ -28,6 +28,6 @@ export class NavElementsComponent {
     this.spinner.show('Logging out, See you soon...');
     this.sessionService.destroySession();
 
-    this.router.navigate(['/authentication']).then(() => location.reload());
+    this.router.navigate(['/authentication']);
   }
 }
