cf24801999198af3ef540105a9c00be312348e8c
ShafiSahal99 <shafisahal99@gmail.com>
2021-08-04 17:21:11 +0530

    optimize: Optimize change Detection

--- src/app/home/bottom-nav/bottom-nav.component.ts
+++ src/app/home/bottom-nav/bottom-nav.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NavService } from '../nav.service';
 
 @Component({
   selector: 'app-bottom-nav',
   templateUrl: './bottom-nav.component.html',
-  styleUrls: ['./bottom-nav.component.scss']
+  styleUrls: ['./bottom-nav.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BottomNavComponent {
   constructor(public navService: NavService) { }
