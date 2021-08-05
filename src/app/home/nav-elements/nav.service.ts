8e9e575770cf15b3ad1d993b7329579e7a1e1ef9
ShafiSahal99 <shafisahal99@gmail.com>
2021-08-05 21:42:17 +0530

    refactor

--- src/app/home/nav-elements/nav.service.ts
+++ src/app/home/nav-elements/nav.service.ts
@@ -4,8 +4,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class NavService {
   navButtons: { label: string, icon: string, route: string }[] = [
-    { label: 'Debates', icon: 'forum', route: '/' },
-    { label: 'Account', icon: 'person' , route: '/account'}
+    { label: 'Debates', icon: 'forum', route: '/' }, { label: 'Account', icon: 'person' , route: '/account'}
   ]
   clickedNavbuttonIndex = 0;
 }
