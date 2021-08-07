b5d64e0fcbd21ed7c85d5c7036304b2853a8e44f
ShafiSahal99 <shafisahal99@gmail.com>
2021-08-07 09:35:28 +0530

    fix: Displaying detailed error on response

--- backend/src/shared/error-handler.js
+++ backend/src/shared/error-handler.js
@@ -1,4 +1,4 @@
 module.exports = (res, error = 'Unknown Error', status = 500) => {
-  console.log(error.errors[0].path.split('.')[1]);
-  res.send({error: error});
+  console.log(error);
+  res.sendStatus(status);
 };
