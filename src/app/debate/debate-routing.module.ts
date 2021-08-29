4ac7f489212378046046eba605cddca1b66e41ad
ShafiSahal99 <shafisahal99@gmail.com>
2021-08-29 22:21:02 +0530

    chore: Added routing to create-a-debate buuton in pc

--- src/app/debate/debate-routing.module.ts
+++ src/app/debate/debate-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DebateCreateComponent } from './debate-create/debate-create.component';
 import { DebateListComponent } from './debate-list/debate-list.component';
 
-const routes: Routes = [ { path: '', component: DebateListComponent } ];
+const routes: Routes = [
+  { path: 'create-debate', component: DebateCreateComponent }, { path: '', component: DebateListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/home/nav-elements/nav.service.ts
+++ src/app/home/nav-elements/nav.service.ts
@@ -3,8 +3,10 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class NavService {
-  navButtons: { label: string, icon: string, route: string }[] = [
-    { label: 'Debates', icon: 'forum', route: '/' }, { label: 'Account', icon: 'person' , route: '/account'}
-  ]
+  navButtons: { label: string, icon?: string, route: string }[] = [
+    { label: 'Debates', icon: 'forum', route: '/' },
+    { label: 'Account', icon: 'person', route: '/account'},
+    { label: 'Create a Debate', route: '/create-debate' }
+  ];
   clickedNavbuttonIndex = 0;
 }
