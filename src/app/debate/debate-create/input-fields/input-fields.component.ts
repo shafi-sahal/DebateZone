27ee5a050a1e53b03aadf529089cd82c1f8bc94a
ShafiSahal99 <shafisahal99@gmail.com>
2021-09-05 16:15:22 +0530

    feat: Change character count color to red if exceeded

--- src/app/debate/debate-create/input-fields/input-fields.component.ts
+++ src/app/debate/debate-create/input-fields/input-fields.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-input-fields',
   templateUrl: './input-fields.component.html',
-  styleUrls: ['./input-fields.component.scss']
+  styleUrls: ['./input-fields.component.scss', '../../../shared/styles/messages.scss']
 })
 export class InputFieldsComponent {
 
