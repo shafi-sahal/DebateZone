3b754c30375ef27cc4f775fabbb767369eaaa37c
ShafiSahal99 <shafisahal99@gmail.com>
2021-09-05 10:19:32 +0530

    refactor: Change parameter name

--- src/app/shared/services/username-error-message.service.ts
+++ src/app/shared/services/username-error-message.service.ts
@@ -3,16 +3,16 @@ import { AbstractControl } from '@angular/forms';
 
 @Injectable()
 export class UsernameErrorMessageService {
-  getErrorMessage(control: AbstractControl | null): string {
-    if (!control) return '';
-    if (control.hasError('isTooSmall')) return 'Username must be atleast 5 characters long';
-    if (control.hasError('isTooBig')) return 'Username must only have a maximum of 30 characters';
-    if (control.hasError('disallowedStartingCharacter')) return 'Username should not start with a period';
-    if (control.hasError('disallowedCharacter')) {
+  getErrorMessage(username: AbstractControl | null): string {
+    if (!username) return '';
+    if (username.hasError('isTooSmall')) return 'Username must be atleast 5 characters long';
+    if (username.hasError('isTooBig')) return 'Username must only have a maximum of 30 characters';
+    if (username.hasError('disallowedStartingCharacter')) return 'Username should not start with a period';
+    if (username.hasError('disallowedCharacter')) {
       return 'Username should only contain letters, numbers, periods, underscores, hyspanhens and @';
     }
-    if (control.hasError('disallowedEndingCharacter')) return 'Username should not end with a period';
-    if (control.hasError('isDuplicateUsername')) return 'Username already taken. Try a different one';
+    if (username.hasError('disallowedEndingCharacter')) return 'Username should not end with a period';
+    if (username.hasError('isDuplicateUsername')) return 'Username already taken. Try a different one';
     return '';
   }
 }
