3975ca0b1bd2d74e68b527456a1bea8650559f57
ShafiSahal99 <shafisahal99@gmail.com>
2021-09-10 20:31:22 +0530

    refactor: Make server error messages more clear to developers

--- backend/src/middleware/user-update-data-validation.js
+++ backend/src/middleware/user-update-data-validation.js
@@ -3,15 +3,21 @@ const errorHandler = require('../shared/error-handler');
 const isMobileFormatCorrect = require('../shared/check-mobile-format');
 
 module.exports = (req, res, next) => {
+
   const mobile = req.body.mobile
   if (mobile) {
-    const isValidMobile = isMobileFormatCorrect(mobile);
-    if (!isValidMobile) return errorHandler(res);
+    if (!isMobileFormatCorrect(mobile)) return errorHandler(res, 'Invalid mobile');
+    req.body = { mobile: mobile };
     return next();
   }
 
-  for (const key of Object.keys(req.body)) {
-    if (!regexes[key].test(req.body[key])) return errorHandler(res);
+  try {
+    for (const key of Object.keys(req.body)) {
+      if (!regexes[key].test(req.body[key])) return errorHandler(res, 'Invalid ' + key);
+    }
+  } catch(error) {
+    return errorHandler(res, error);
   }
+
   next();
 }
