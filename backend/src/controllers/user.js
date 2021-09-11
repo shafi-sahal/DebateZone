24b9a530421c0e12cd15ae447cfd3186ed8e65d5
ShafiSahal99 <shafisahal99@gmail.com>
2021-09-11 19:37:02 +0530

    refactor: Use async/await instaed of promises

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -5,74 +5,86 @@ const bcrypt = require('bcrypt');
 const { createSigner } = require('fast-jwt');
 const user = require('../models/user');
 
-exports.isDuplicate = (req, res) => {
+exports.isDuplicate = async (req, res) => {
   const query = req.query;
   if (!(query.username || query.email || query.mobile)) return errorHandler(res);
-  checkUserExistence(query).then(hasUser => res.send(hasUser));
+  const user = await checkUserExistence(query);
+  res.send(!!user);
 }
 
-exports.createUser = (req, res) => {
-  User.create(req.body).then(user => {
+exports.createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
     const token = generateToken({ userId: user.id });
     res.status(201).json({ token: token, user: { name: user.name, username: user.username }});
-  })
-  .catch(error => {
+  } catch(error) {
     if (error.name !== 'SequelizeUniqueConstraintError') return errorHandler(res, error)
     message = handleDuplicateUserErrors(error);
     res.status(400).json({ message: message });
-  });
+  }
 }
 
-exports.login = (req, res) => {
+exports.login = async (req, res) => {
   const loginKey = req.body.loginKey;
   const password = req.body.password;
-  if (!(loginKey && password)) return errorHandler(res);
-  const pepper = process.env.PEPPER;
-  let fetchedUser;
+  if (!loginKey || !password) return errorHandler(res, 'loginKey and password is required for login');
 
-  User.findOne({
-    attributes: ['id', 'name', 'username', 'password'],
-    where: {
-      [Sequelize.Op.or]: [ { email: loginKey }, { username: loginKey }, { mobile: loginKey } ]
-    }
-  })
-  .then(user => {
-    if (!user) throw(404);
-    fetchedUser = user;
+  try {
+    const user = await User.findOne({
+      attributes: ['id', 'name', 'username', 'password'],
+      where: {
+        [Sequelize.Op.or]: [ { email: loginKey }, { username: loginKey }, { mobile: loginKey } ]
+      }
+    });
+
+    if(!user) return errorHandler(res, 'Unauthorized (User does not exist)', 401);
     const pepper = process.env.PEPPER;
-    return bcrypt.compare(password + pepper, user.password)
-  })
-  .then(isMatching => {
-    if (!isMatching) return res.sendStatus(401);
-    const token = generateToken({ userId: fetchedUser.id });
-    res.json({ token: token, user: { name: fetchedUser.name, username: fetchedUser.username } })
-  })
-  .catch(error => {
-    /*
-      A 404 error code is turned to 401 due to security reasons.
-      The client should not be able to tell whether the username or password is wrong specifically to make it more difficult for
-      unauthorized access.
-      This behaviuor does not decrease user friendliness as users can login using their email, username or mobile.
-    */
-    if (error === 404) errorHandler(res, error, 401); else errorHandler(res, error);
-  });
+    const isPasswordMatching = await bcrypt.compare(password + pepper, user.password);
+    if (!isPasswordMatching) return errorHandler(res, 'Unauthorized (Password mismatch)', 401);
+    const token = generateToken({ userId: user.id });
+    res.json({ token: token, user: { name: user.name, username: user.username } });
+  } catch(error) {
+    errorHandler(res, error);
+  }
 }
 
-exports.fetchUser = (req, res) => {
-  User.findOne(({ attributes: ['name', 'username', 'email', 'mobile'], where: { id: req.userId } }))
-    .then(user => res.send(user)).catch(error => errorHandler(res, error)
+exports.fetchUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ attributes: ['name', 'username', 'email', 'mobile'], where: { id: req.userId } });
+    res.send(user);
+  } catch(error) {
+    errorHandler(res, error);
+  }
+}
+
+exports.fetchUsers = (req, res) => {
+  const query = req.query.query;
+  let fetchedUsers = [];
+  searchUsers('%' +query + '%')
+    .then(users => {
+      if (users.length === 9) return res.send(users);
+      else {
+        fetchedUsers.push(users);
+        return searchUsers('% ' + query + '%');
+      }
+    })
+    .then(users => {
+      fetchedUsers.push(users);
+      res.send(fetchedUsers);
+    })
+    .catch(error => errorHandler(res, error)
   );
 }
 
-exports.updateUser = (req, res) => {
-  User.update(req.body, { where: { id: req.userId } })
-    .then(() => res.sendStatus(204))
-    .catch(error => {
-      if (error.name !== 'SequelizeUniqueConstraintError') return errorHandler(res, error)
-      message = handleDuplicateUserErrors(error);
-      res.status(400).json({ message: message });
-    }
-  );
+exports.updateUser = async (req, res) => {
+  try {
+    await User.update(req.body, { where: { id: req.userId } });
+    res.sendStatus(204);
+  } catch(error) {
+    if (error.name !== 'SequelizeUniqueConstraintError') return errorHandler(res, error)
+    message = handleDuplicateUserErrors(error);
+    res.status(400).json({ message: message });
+  }
 }
 
 const handleDuplicateUserErrors = error => {
@@ -90,13 +102,9 @@ const handleDuplicateUserErrors = error => {
   return message;
 }
 
-const checkUserExistence = query => {
+const checkUserExistence = async query => {
   const conditionKey = Object.keys(query)[0];
-  return new Promise(resolve => {
-    User.findOne({ attributes: ['id'], where: { [conditionKey]: query[conditionKey] } }).then(user => {
-      resolve(!!user);
-    });
-  });
+  return await User.findOne({ attributes: ['id'], where: { [conditionKey]: query[conditionKey] } });
 }
 
 const capitalizeFirstLetter = word => {
@@ -108,4 +116,19 @@ const capitalizeFirstLetter = word => {
 const generateToken = payload => {
   const sign = createSigner({ key: process.env.JWT_SECRET });
   return sign(payload);
+}
+
+const searchUsers = likeQuery => {
+  return new Promise(resolve => {
+    User.findAll({
+      attributes: ['name', 'username'],
+      where: {
+        [Sequelize.Op.or]: [
+           { name: { [Sequelize.Op.like]: likeQuery }}, { username: { [Sequelize.Op.like]: likeQuery } }
+        ]
+      },
+      limit: 9
+    })
+    .then(users => resolve(users));
+  });
 }
