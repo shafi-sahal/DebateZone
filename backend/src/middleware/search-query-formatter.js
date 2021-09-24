b60cc47632ee96d47cefe501a966118d88f5c161
ShafiSahal99 <shafisahal99@gmail.com>
2021-09-24 09:10:33 +0530

    chore: Validate search queries

--- backend/src/middleware/search-query-formatter.js
+++ backend/src/middleware/search-query-formatter.js
@@ -1,16 +1,24 @@
 'use strict';
+const errorHandler = require('../shared/error-handler');
+const regexes = require('../shared/regexes');
 
 module.exports = (req, res, next) => {
   let query = req.query.query;
   const hasAmpersand = query.includes('@');
   if (hasAmpersand) query = query.replace('@','');
   const hasUnderscore = query.includes('_');
+  const hasFullStop = query.includes('.');
+  const isUsernameQuery = hasAmpersand || hasUnderscore || hasFullStop;
+
+  const regex = isUsernameQuery ? regexes.usernameSearchTerm : regexes.searchTerm;
+  if (!regex.test(query)) return errorHandler(res, new Error('Bad search'));
+
   const isQueryStartWithUnderscore = query.charAt(0) === '_';
   if (isQueryStartWithUnderscore) query = '\\' + query;
   const isQueryEndWithUnderscore = query.charAt(query.length - 1) === '_';
   if (isQueryEndWithUnderscore) query = query.slice(0, -1) + '\\_';
   req.query.query = query;
-  req.isUsernameQuery = hasAmpersand || hasUnderscore;
+  req.isUsernameQuery = isUsernameQuery;
   req.isQueryStartWithUnderscore = isQueryStartWithUnderscore;
   next();
 }
